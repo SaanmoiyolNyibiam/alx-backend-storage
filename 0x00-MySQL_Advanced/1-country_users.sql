994bf461f418cb61454f6f300175c78c82e17e40
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-17 12:13:10 +0100

    build(alx-b-storage:0x00): add solution to task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,16 @@
+-- This is an sql script that creates a table called users
+-- with the following attributes
+-- id, integer, never null, auto increment and primary key
+-- * email, string (255 characters), never null and unique
+-- * name, string (255 characters)
+-- * country, enumeration of countries: US, CO and TN, 
+-- * never null (= default will be the first element 
+--   of the enumeration, here US)
+
+CREATE TABLE IF NOT EXISTS users(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		email CHAR(255) NOT NULL UNIQUE,
+		name char(255),
+		country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+		);
+
