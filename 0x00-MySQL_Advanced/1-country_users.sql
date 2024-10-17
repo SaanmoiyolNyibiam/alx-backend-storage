ba85a17acc3cc07bf9a46ea1a409199321d02dc8
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-17 14:35:39 +0100

    style(alx-b-storage:0x00): use caps for all keywords

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -10,7 +10,7 @@
 CREATE TABLE IF NOT EXISTS users(
 		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		email CHAR(255) NOT NULL UNIQUE,
-		name char(255),
+		name CHAR(255),
 		country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 		);
 
