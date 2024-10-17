afb6fb3cba47796f7871baab2cf4f1adf84338a9
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-17 10:08:31 +0100

    build(alx-b-storage:0x00): add solution to task 1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- This is an sql script that creates a table called users
+
+CREATE TABLE IF NOT EXISTS users(
+		id INT NOT NULL AUTO_INCREMENT,
+		email CHAR(255),
+		name char(255),
+		PRIMARY KEY(id)
+		);
+
