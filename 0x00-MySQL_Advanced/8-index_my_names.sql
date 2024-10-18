282bbef10531fc76f9750aa65676a50fbbf5ac3a
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-18 18:01:28 +0100

    refactor(alx-b-storage:0x00): add index for only first letters of columns starting with 'a'

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,8 +1,9 @@
 -- This is an SQL script that creates an index idx_name_first
 -- on the table names and the first letter of name.
 
--- DROP INDEX IF EXISTS idx_name_first ON names;
+-- DROP INDEX IF EXISTS idx_name_first;
 
 CREATE INDEX idx_name_first
-ON names (name);
+ON names (name)
+WHERE name LIKE 'a%';
 
