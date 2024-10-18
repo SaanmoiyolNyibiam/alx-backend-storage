860c62987548f078bac1d13456cebb03f0c1dcec
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-18 18:14:35 +0100

    build(alx-b-storage:0x00): add solution to task 9

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,8 @@
+-- This is an SQL script that creates an index idx_name_first
+-- on the table names and the first letter of name.
+
+-- DROP INDEX IF EXISTS;
+
+CREATE INDEX idx_name_first_score
+ON names (name(1), score);
+
