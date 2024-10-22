b8804917a1629504d54f2130870b676c7aa1e519
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-22 15:18:18 +0100

    build(alx-b-storage:0x01): add solution to task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This is a Python script that provides some
+stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+# establish connection to database
+client = MongoClient("mongodb://localhost:27017")
+
+# retrieve cursor object to database and collection
+db = client.logs
+docs = db.nginx
+
+# query collection and return required data
+number_of_docs = docs.count_documents({})
+number_of_gets = docs.count_documents({"method": "GET"})
+number_of_posts = docs.count_documents({"method": "POST"})
+number_of_puts = docs.count_documents({"method": "PUT"})
+number_of_patches = docs.count_documents({"method": "PATCH"})
+number_of_deletes = docs.count_documents({"method": "DELETE"})
+number_of_mget_pstatus = docs.count_documents({"method": 'GET', "path": "/status"})
+
+# print output based on sample format provided
+print(f"{number_of_docs} logs")
+
+print(f"Methods:\n\
+      \t method GET: {number_of_gets}\n\
+      \t method POST: {number_of_posts}\n\
+      \t method PUT: {number_of_puts}\n\
+      \t method PATCH: {number_of_patches}\n\
+      \t method DELETE: {number_of_deletes}")
+
+print(f"{number_of_mget_pstatus} status check")
