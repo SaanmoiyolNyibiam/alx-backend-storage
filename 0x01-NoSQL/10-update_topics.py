88d899ca68a54d9523f784375d751e7521f47952
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-22 12:49:39 +0100

    build(alx-b-storage:0x01): add solution to task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a update_topics function
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    This is a function that changes all topics
+    of a school document based on the name
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
