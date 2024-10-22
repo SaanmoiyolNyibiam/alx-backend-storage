a9aa200c38f45c5811788c8120886cbb1032c00b
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-22 13:26:12 +0100

    build(alx-b-storage:0x01): add solution to task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a schools_by_topic function
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    This is a function that returns the list of schools having
+    a specific topic
+    """
+    docs = mongo_collection.find({"topics": topic})
+
+    return docs
