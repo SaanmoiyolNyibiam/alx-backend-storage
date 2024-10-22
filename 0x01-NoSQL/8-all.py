46123140e9b76d77ef95369502d2961a189e87f6
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-22 12:08:06 +0100

    docs(alx-b-strorage:0x01): add docstring to list_all function

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
 """
 This is a module that defines a list_all function
 """
 
-def list_all(mongo_collection):
-	return mongo_collection.find()
 
+def list_all(mongo_collection):
+    """
+	This is a function that lists all documents in a collection
+    """
+    return mongo_collection.find()
