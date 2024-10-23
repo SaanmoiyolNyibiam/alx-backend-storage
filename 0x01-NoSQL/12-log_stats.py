bfdc72ccbae8f3dbadd5034ee9d15e5b5364538f
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-23 13:03:23 +0100

    refactor(alx-b-storage:0x01): resturucture algorithm to use a log_stats function to make code modular

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,34 +3,37 @@
 This is a Python script that provides some
 stats about Nginx logs stored in MongoDB
 """
-from pymongo import MongoClient
 
-# establish connection to database
-client = MongoClient("mongodb://localhost:27017")
+def log_stats(docs):
+    """
+    This is a function that logs stats stored in a
+    logs collection in mongodb
+    """
+    number_of_docs = docs.count_documents({})
+    number_of_gets = docs.count_documents({"method": "GET"})
+    number_of_posts = docs.count_documents({"method": "POST"})
+    number_of_puts = docs.count_documents({"method": "PUT"})
+    number_of_patches = docs.count_documents({"method": "PATCH"})
+    number_of_deletes = docs.count_documents({"method": "DELETE"})
+    number_of_mget_pstatus = docs.count_documents({"method": 'GET', "path": "/status"})
 
-# retrieve cursor object to database and collection
-db = client.logs
-docs = db.nginx
+    print(f"{number_of_docs} logs")
 
-# query collection and return required data
-number_of_docs = docs.count_documents({})
-number_of_gets = docs.count_documents({"method": "GET"})
-number_of_posts = docs.count_documents({"method": "POST"})
-number_of_puts = docs.count_documents({"method": "PUT"})
-number_of_patches = docs.count_documents({"method": "PATCH"})
-number_of_deletes = docs.count_documents({"method": "DELETE"})
-number_of_mget_pstatus = docs.count_documents({"method": 'GET', "path": "/status"})
+    print(f"Methods:\n"
+          f"    method GET: {number_of_gets}\n"
+          f"    method POST: {number_of_posts}\n"
+          f"    method PUT: {number_of_puts}\n"
+          f"    method PATCH: {number_of_patches}\n"
+          f"    method DELETE: {number_of_deletes}")
 
-# print output based on sample format provided
-print(f"{number_of_docs} logs")
+    print(f"{number_of_mget_pstatus} status check")
 
-print(f"Methods:\n"
-      f"    method GET: {number_of_gets}\n"
-      f"    method POST: {number_of_posts}\n"
-      f"    method PUT: {number_of_puts}\n"
-      f"    method PATCH: {number_of_patches}\n"
-      f"    method DELETE: {number_of_deletes}")
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
-print(f"{number_of_mget_pstatus} status check")
+    client = MongoClient("mongodb://localhost:27017")
 
-client.close()
+    """retrieve cursor object to database and collection"""
+    db = client.logs
+    docs_collection = db.nginx
+    log_stats(docs_collection)
