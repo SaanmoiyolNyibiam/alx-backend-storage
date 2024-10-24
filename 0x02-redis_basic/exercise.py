9c4fd95f1f1a85e3b13ae37df1687070a30501ce
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-24 12:02:40 +0100

    build(alx-b-storage:0x02): add solution to task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This is module that defines a Cache class"""
+from typing import Union
+import uuid
+import redis
+
+
+class Cache():
+    """This is a class that defines a Cache"""
+    def __init__(self):
+        """Initializes the Cache class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        This is a method that method that takes a data argument and
+        returns a string. The method should generate a random key (e.g. using uuid),
+        store the input data in Redis using the random key and return the key.
+        """
+        rand_key = str(uuid.uuid4())
+        self._redis.set(rand_key, data)
+        return rand_key
