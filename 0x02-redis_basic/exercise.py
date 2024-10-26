a55629089d72f6d64f22438ed4576bac4b31509d
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-26 13:21:04 +0100

    build(alx-b-storage:0x02): add get, get_str and get_int methods to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """This is module that defines a Cache class"""
-from typing import Union
+from typing import Union, Callable
 import uuid
 import redis
 
@@ -22,3 +22,32 @@ class Cache():
         rand_key = str(uuid.uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Callable=None) -> any:
+        """
+        This is a method that take a key string argument and
+        an optional Callable argument named fn. This callable
+        will be used to convert the data back to the desired format.
+        """
+        key_value = self._redis.get(key)
+
+        # Validate and retrieve value
+        if fn is None:
+            return key_value
+        desired_format = fn(key_value)
+
+        return desired_format
+
+    def get_str(self, key: str) -> str:
+        """
+        Automatically parametrizes Cache.get to
+        return a string
+        """
+        return str(self.get(key, lambda v: v.decode("utf-8")))
+
+    def get_int(self, key: str) -> int:
+        """
+        Automatically parametrizes Cache.get to
+        return an int
+        """
+        return self.get(key, int)
